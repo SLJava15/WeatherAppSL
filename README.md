67819116ec609aa9535c81e74ece97120ca7acde
Sulev_L <sulev.lohmus@gmail.com>
2022-05-06 21:58:59 +0300

    Added my project

---
+++ README.md
@@ -0,0 +1,24 @@
+# WeatherApp
+
+A weather forecast app, search for a Zip Code to get the Weather FOrecast for that city.
+
+## Requirements
+
+* NodeJS 8.0 or greater (https://nodejs.org/es/)
+* Angular CLI (npm install -g @angular/cli)
+
+## Setup
+
+Run `npn install`
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Config
+
+Change API and API Keys on environments folder to use different access key and weather API.
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  weatherForecast: {
+    apiUrl: 'https://samples.openweathermap.org/data/2.5/forecast',
+    apiKey: 'b6907d289e10d714a6e88b30761fae22'
+  },
+  googleMaps: {
+    apiUrl: 'https://maps.googleapis.com/maps/api/timezone/json',
+    apiKey: 'AIzaSyD7cZtGm_DZca0AqWFvGRxWoQD-AodQhFs'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
